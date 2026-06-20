fe45074375f47481ab119ecca19d6d88d1761217
Martin J. Pollard <martin0566@gmail.com>
2026-06-20 00:11:18 -0400

    sdk: support: Update to use SPDX headers

--- support/src/com/lineage/support/colorpicker/ColorPickerPreference.java
+++ support/src/com/lineage/support/colorpicker/ColorPickerPreference.java
@@ -1,21 +1,10 @@
 /*
- * Copyright (C) 2011 Sergey Margaritov
- * Copyright (C) 2013 Slimroms
- * Copyright (C) 2015 The TeamEos Project
- * Copyright (C) 2020-2021 crDroid Android Project
- * Copyright (C) 2022 Altair ROM Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-FileCopyrightText: Sergey Margaritov
+ * SPDX-FileCopyrightText: Slimroms
+ * SPDX-FileCopyrightText: The TeamEos Project
+ * SPDX-FileCopyrightText: crDroid Android Project
+ * SPDX-FileCopyrightText: Altair ROM Project
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.lineage.support.colorpicker;
